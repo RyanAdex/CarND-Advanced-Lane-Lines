684dc3a3d496ed7baa46bc29f84f14c0f1614232
Ryan <ryan-mask@outlook.com>
2019-01-28 16:08:55 +0800

    add cmakelists.txt

---
+++ build/CMakeFiles/abc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abc.dir/Lane_Finding_cpp.cpp.obj"
+  "../bin/abc.pdb"
+  "../bin/abc.exe"
+  "../bin/abc.exe.manifest"
+  "../bin/libabc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/abc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
